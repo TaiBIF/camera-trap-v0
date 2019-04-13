5823a7a88ddbbdbd9edc129d2af664a6578a923e
Chihjen Ko <cjk.halodule@gmail.com>
2019-04-13 13:55:20 +0800

    Production datasources.

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -1,5 +1,13 @@
 module.exports = {
   ctMongoDb40: {
     host: 'localhost',
+    port: 27017,
+    url: 'mongodb://localhost:27017/ctp',
+    database: 'ctp',
+    password: '',
+    name: 'ctMongoDb40',
+    user: '',
+    connector: 'mongodb',
+    useNewUrlParser: 'true',
   },
 };
